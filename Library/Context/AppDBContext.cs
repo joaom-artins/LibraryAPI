1ea9b6b1506c5b6c3138fe9499e019dc61760ca6
Joao Martins <milanezijoao51@gmail.com>
2023-12-27 13:07:53 -0300

    Conexão com o banco de dados,criação da classe de contexto e mapemanto do banco

---
+++ Library/Context/AppDBContext.cs
@@ -0,0 +1,22 @@
+﻿using Library.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Context
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+        }
+        public DbSet<Book>? Books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<Book>().HasKey(i => i.ID); 
+            mb.Entity<Book>().Property(i=>i.Title).HasMaxLength(125).IsRequired();
+            mb.Entity<Book>().Property(i => i.Description).HasMaxLength(255).IsRequired();
+            mb.Entity<Book>().Property(i => i.Author).HasMaxLength(125);
+            mb.Entity<Book>().Property(i => i.Price).HasPrecision(10, 2).IsRequired();
+        }
+    }
+}
---
+++ Library/Migrations/20231227134224_migracaoInicial.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Library.Migrations
+{
+    public partial class migracaoInicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(type: "varchar(125)", maxLength: 125, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Pages = table.Column<int>(type: "int", nullable: false),
+                    Author = table.Column<string>(type: "varchar(125)", maxLength: 125, nullable: true)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Price = table.Column<decimal>(type: "decimal(10,2)", precision: 10, scale: 2, nullable: false),
+                    Description = table.Column<string>(type: "varchar(255)", maxLength: 255, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.ID);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
