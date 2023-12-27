d9623fa244cefc88efcaf88f6d78788b601ccd5d
Joao Martins <milanezijoao51@gmail.com>
2023-12-27 09:18:21 -0300

    Adicionando dependencias e criando a primeira model

---
+++ Library/Model/Book.cs
@@ -0,0 +1,14 @@
+﻿namespace Library.Model
+{
+    public class Book
+    {
+        public int ID { get; set; }
+        public string? Title { get; set; }
+        public int Pages { get; set; }
+        public string? Author { get; set; }
+        public decimal Price { get; set; }
+        public string? Description { get; set; }
+
+
+    }
+}
